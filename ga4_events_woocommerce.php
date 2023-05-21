5ad88ca7c387907f59b79bb42aaef5655238c303
edouardproust <sygnostudio@protonmail.com>
2023-05-22 01:30:19 +0200

    ga4 events woo

--- ga4_events_woocommerce.php
+++ ga4_events_woocommerce.php
@@ -1,95 +1,179 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-add_action('head', 'ga4_config');
-add_action('woocommerce_before_single_product', 'ga4_view_item', 10, 1);
-add_action('woocommerce_add_to_cart', 'ga4_add_to_cart');
-add_action('woocommerce_before_checkout_form', 'ga4_begin_checkout');
-add_action('woocommerce_thankyou', 'ga4_purchase');
+const GA4_MEASUREMENT_ID = 'G-PZ5MNKZ88B';
+
+add_action('wp_head', 'ga4_config');
+add_action('woocommerce_before_shop_loop', 'ga4_view_item_list');
+add_action('woocommerce_before_single_product', 'ga4_view_item');
+add_action('woocommerce_add_to_cart', 'ga4_add_to_cart', 10, 3);
 add_action('woocommerce_before_cart', 'ga4_view_cart');
+add_action('woocommerce_before_checkout_form', 'ga4_begin_checkout');
+add_action('woocommerce_thankyou', 'ga4_purchase', 10, 1);
 
-function ga4_config() {
-?>
+function ga4_config()
+{
+    ?>
+<script async
+    src="https://www.googletagmanager.com/gtag/js?id=<?= GA4_MEASUREMENT_ID ?>">
+</script>
 <script>
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-PZ5MNKZ88B"></script>
-<script>
- window.dataLayer = window.dataLayer || [];
- function gtag(){dataLayer.push(arguments);}
- gtag('js', new Date());
- gtag('config', 'G-PZ5MNKZ88B');
-</script><!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-P6ZSBHC');</script>
-<!-- End Google Tag Manager -->
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+    gtag('config', '<?= GA4_MEASUREMENT_ID ?>');
+</script>
 <?php
 }
 
-function ga4_view_item() {
-global $product;
-if (!$product) {
+function ga4_view_item_list()
+{
+    $product_query = new WP_Query(array(
+        'post_type' => 'product',
+        'posts_per_page' => -1,
+    ));
+    $products = $product_query->get_posts(); ?>
+<script>
+    gtag('event', 'view_item_list', {
+        currency: '<?= get_woocommerce_currency() ?>',
+        items: [
+            <?php foreach ($products as $product): ?>
+            <?php $wc_product = wc_get_product($product->ID); ?>
+            {
+                item_id: '<?= $product->ID ?>',
+                item_name: '<?= $product->post_title ?>',
+                price: <?= $wc_product->get_price() ?> ,
+                quantity: 1,
+            },
+            <?php endforeach ?>
+        ],
+    });
+</script>
+<?php
+}
+
+
+function ga4_view_item()
+{
+    global $product;
+    if (!$product) {
         return;
     }
-?>
+    ?>
 <script>
-gtag('event', 'view_item', {
-value: <?= $product->get_price() ?>,
-currency: '<?= get_woocommerce_currency() ?>',
-items: [
+    gtag('event', 'view_item', {
+        value: <?= $product->get_price() ?> ,
+        currency: '<?= get_woocommerce_currency() ?>',
+        items: [{
+            item_id: '<?= $product->get_id() ?>',
+            item_name: '<?= $product->get_name() ?>',
+            price: <?= $product->get_price() ?> ,
+        }],
+    });
+</script>
+<?php
+}
+
+function ga4_add_to_cart($cart_item_key, $product_id, $quantity)
 {
-item_id: '<?= $product->get_id() ?>',
-item_name: '<?= $product->get_name() ?>',
-price: <?= $product->get_price() ?>,
-}
-],
-});
-</script>
-<?php
-}
-
-function ga4_add_to_cart() {
-?>
+    $product = wc_get_product($product_id);
+    $price = $product->get_price();
+    ?>
 <script>
-const wait_for_config = setInterval(function(){
-if(typeof gtag === 'function') {
-gtag('event', 'add_to_cart', {
-
-});
-clearInterval(wait_for_config);
-}
-}, 20);
+    const waitForGtag = setInterval(function() {
+        if (typeof gtag === 'function') {
+            gtag('event', 'add_to_cart', {
+                currency: '<?= get_woocommerce_currency() ?>',
+                value: <?= $price * $quantity ?> ,
+                items: [{
+                    item_id: '<?= $product->get_id() ?>',
+                    item_name: '<?= $product->get_name() ?>',
+                    price: <?= $price ?> ,
+                    quantity: <?= $quantity ?> ,
+                }]
+            });
+            clearInterval(waitForGtag);
+        }
+    }, 100);
 </script>
 <?php
 }
 
-function ga4_view_cart() {
-?>
+function ga4_view_cart()
+{
+    $cart_items = WC()->cart->get_cart();
+    ?>
 <script>
-gtag('event', 'view_cart', {
-
-});
+    gtag('event', 'view_cart', {
+        currency: '<?= get_woocommerce_currency() ?>',
+        value: <?= WC()->cart->subtotal ?> ,
+        items: [
+            <?php foreach ($cart_items as $cart_item): ?>
+            <?php $product = $cart_item['data'] ?>
+            {
+                item_id: '<?= $product->get_id() ?>',
+                item_name: '<?= $product->get_name() ?>',
+                price: <?= $product->get_price() ?> ,
+                quantity: <?= $cart_item['quantity'] ?> ,
+            },
+            <?php endforeach ?>
+        ],
+    });
 </script>
 <?php
 }
 
-function ga4_begin_checkout() {
-?>
+
+function ga4_begin_checkout()
+{
+    $cart_items = WC()->cart->get_cart();
+    ?>
 <script>
-gtag('event', 'begin_checkout', {
-
-});
+    gtag('event', 'begin_checkout', {
+        currency: '<?= get_woocommerce_currency() ?>',
+        value: <?= WC()->cart->subtotal ?> ,
+        items: [
+            <?php foreach ($cart_items as $cart_item): ?>
+            <?php $product = $cart_item['data'] ?>
+            {
+                item_id: '<?= $product->get_id() ?>',
+                item_name: '<?= $product->get_name() ?>',
+                price: <?= $product->get_price() ?> ,
+                quantity: <?= $cart_item['quantity'] ?> ,
+            },
+            <?php endforeach ?>
+        ],
+    });
 </script>
 <?php
 }
 
-function ga4_purchase() {
-?>
+function ga4_purchase($order_id)
+{
+    $order = wc_get_order($order_id);
+    ?>
 <script>
-gtag('event', 'purchase', {
-
-});
+    gtag('event', 'purchase', {
+        transaction_id: '<?= $order->get_order_number() ?>',
+        value: <?= $order->get_subtotal() ?> ,
+        currency: '<?= $order->get_currency() ?>',
+        items: [
+            <?php foreach ($order->get_items() as $item): ?>
+            {
+                $item_id: <?= $item->get_id() ?> ,
+                $item_name: <?= $item->get_name() ?> ,
+                $item_quantity: <?= $item->get_quantity() ?> ,
+            },
+            <?php endforeach ?>
+        ],
+    });
 </script>
 <?php
 }
+
+?>
